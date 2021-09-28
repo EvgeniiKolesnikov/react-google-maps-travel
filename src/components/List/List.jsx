42526c0bee60e7c7e2ee94356ae0ec8d2df7e666
Evgenii Kolesnikov <jekow@yandex.ru>
2021-09-28 18:24:33 +0300

    Add components structure

---
+++ src/components/List/List.jsx
@@ -0,0 +1,11 @@
+import styles from './List.module.scss';
+
+const List = () => {
+  return ( 
+    <div>
+      List
+    </div> 
+  );
+};
+
+export default List;
---
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -0,0 +1,11 @@
+import styles from './PlaceDetails.module.scss';
+
+const PlaceDetails = () => {
+  return ( 
+    <div>
+      PlaceDetails
+    </div> 
+  );
+};
+
+export default PlaceDetails;
