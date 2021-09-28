916cf908526dd70b717672f0cf1cee6b478562c2
Evgenii Kolesnikov <jekow@yandex.ru>
2021-09-28 19:44:20 +0300

    Config Header and Map components

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,30 @@
+import { useMediaQuery } from '@material-ui/core';
+import GoogleMapReact from 'google-map-react';
+import { MAPS_API_KEY } from '../../config';
 import styles from './Map.module.scss';
 
+import useStyles from './styles';
+
 const Map = () => {
-  return ( 
-    <div>
-      Map
-    </div> 
+  const classes = useStyles();
+  const isMobile = useMediaQuery('min-width:600px');
+
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: MAPS_API_KEY }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        // options={''}
+        // onChange={''}
+        // onChildClick={''}
+      >
+      </GoogleMapReact>
+    </div>
   );
 };
 
---
+++ src/config.js
@@ -0,0 +1,3 @@
+const MAPS_API_KEY = process.env.REACT_APP_MAPS_API_KEY;
+
+export { MAPS_API_KEY };
